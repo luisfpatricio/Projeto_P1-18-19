ed9d55f453273245e8f360d7b1b470389e47a901
luisfpatricio <luisf.patricio@hotmail.com>
2018-12-17 21:56:06 +0000

    update

--- main.c
+++ main.c
@@ -1,7 +1,7 @@
- /*
-    Luís Patrício - nº 2162357
-    Projecto Programação I - Gestão de Bicicletas no IPL
-    ©2018-19 - Engenharia Informática - ESTG, IPLeiria
+/*
+   Luís Patrício - nº 2162357
+   Projecto Programação I - Gestão de Bicicletas no IPL
+   ©2018-19 - Engenharia Informática - ESTG, IPLeiria
 */
 #include <stdio.h>
 #include <stdlib.h>
@@ -12,6 +12,53 @@
 
 int main()
 {
+    int opcaoMenuPrincipal, opcaoMenuBicicletas, opcaoMenuUtentes, opcaoMenuEmprestimos, opcaoMenuListaEspera, opcaoMenuFicheiros;
+
+    // Chama Menu Principal
+    do
+    {
+        opcaoMenuPrincipal=menuPrincipal();
+
+        switch(opcaoMenuPrincipal)
+        {
+        case '1':
+            //menu Bicicletas
+            do
+            {
+                opcaoMenuBicicletas = menuBicicletas();
+            }while(opcaoMenuBicicletas != 0);
+
+            break;
+        case '2':
+            //Menu Utentes
+            do
+            {
+                opcaoMenu
+            }
+            menuUtentes();
+            break;
+        case '3':
+            //Menu Emprestimos
+            menuEmprestimos();
+            break;
+        case '4':
+            //Menu Lista de Espera
+
+            break;
+        case '5':
+            //MenuFicheiros
+            menuFicheiros();
+            break;
+        case '0':
+            //Sair;
+            printf("/n/nAdeus!!!");
+            break;
+        /*
+        default:
+            printf("/nOpcao Errada!");
+        */
+        }
+    }while(opcaoMenuPrincipal != 0);
 
 
     return 0;
